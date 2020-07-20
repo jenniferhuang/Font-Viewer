6e21200f78434017e9a395ac49a7e57d5be25c39
Jennifer Huang <jennifer.huang@ringcentral.com>
2020-07-20 23:48:21 +0800

    init commit

---
+++ Font Viewer/FontsDisplayViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FontsDisplayViewController.swift
+//  Font Viewer
+//
+//  Created by Jennifer Huang on 2020/7/20.
+//  Copyright © 2020 Jennifer Huang. All rights reserved.
+//
+
+import Cocoa
+
+class FontsDisplayViewController: NSViewController {
+    @IBOutlet var fontsTextView: NSTextView!
+    
+    @IBAction func closeWindow(_sender:Any){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
